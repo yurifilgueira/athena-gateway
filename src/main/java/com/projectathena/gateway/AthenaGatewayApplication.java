886375e024ce8fe0602c0b0c71bfd486f97e9e10
yurifilgueira <yurimedeiros141@gmail.com>
2025-10-08 16:25:29 -0300

    Initial commit

---
+++ src/main/java/com/projectathena/gateway/AthenaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.projectathena.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectathena/gateway/AthenaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectathena.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
