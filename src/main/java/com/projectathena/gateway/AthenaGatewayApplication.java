1141142a51b7336bde5bba4e6a4d9a0d7ae45038
yurifilgueira <yurimedeiros141@gmail.com>
2025-10-15 22:42:32 -0300

    feat: Enable dynamic routing

--- src/main/java/com/projectathena/gateway/AthenaGatewayApplication.java
+++ src/main/java/com/projectathena/gateway/AthenaGatewayApplication.java
@@ -2,12 +2,12 @@ package com.projectathena.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AthenaGatewayApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(AthenaGatewayApplication.class, args);
     }
-
 }
